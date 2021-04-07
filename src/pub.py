3f3bf89d48dcf607e376ec5c84ce41e9bc919790
klamb95 <klamb1995@gmail.com>
2021-04-07 14:33:03 +0100

    take customer money

--- src/pub.py
+++ src/pub.py
@@ -1,3 +1,4 @@
+
 class Pub:
     def __init__(self, pub_name, till):
         self.pub_name = pub_name 
@@ -6,3 +7,19 @@ class Pub:
 
     def stock_count(self):
         return len(self.drinks_list)
+
+    def check_customer_wallet(self, customer, drink):
+        return customer.wallet >= drink.price
+
+    def take_customer_money(self, customer, drink):
+        if self.check_customer_wallet(customer, drink) == True:
+            customer.wallet -= drink.price
+            return customer.wallet
+
+    # def sell_drink_to_customer(self, customer, drink):
+    #    drink = self.(drink_name)
+    #    customer.reduce_cash(pet.price)
+    #    self.increase_total_cash(pet.price)
+    #    self.increase_pets_sold()
+    #    self.remove_pet(pet)
+    #    customer.add_pet(pet)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,10 +1,15 @@
 import unittest
 from src.pub import Pub
+from src.customer import Customer
+from src.drink import Drink
 
 class TestPub(unittest.TestCase):
     
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
+        self.customer_1 = Customer("Ben", 10)
+        self.customer_2 = Customer("Adam", 1)
+        self.drink = Drink("Tennents", 2)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.pub_name)
@@ -16,6 +21,19 @@ class TestPub(unittest.TestCase):
     #@unittest.skip("Delete this line to run the test")
     def test_pub_has_drinks(self):
         self.assertEqual(0, self.pub.stock_count())
+
+    #@unittest.skip("Delete this line to run the test")
+    def test_customer_has_money(self):
+        self.assertEqual(True, self.pub.check_customer_wallet(self.customer_1, self.drink))
+
+    #@unittest.skip("Delete this line to run the test"
+    def test_customer_has_money(self):
+        self.assertEqual(False, self.pub.check_customer_wallet(self.customer_2, self.drink))
+
+    def test_customer_money_reduced(self):
+        self.assertEqual(8, self.pub.take_customer_money(self.customer_1, self.drink))
+    
+    
 
 
 
